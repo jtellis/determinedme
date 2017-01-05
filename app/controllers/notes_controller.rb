f1518dd95d69757c0c4b467280621f1bd1e26f44
Jesse Ellis <jessetellis@gmail.com>
2017-01-05 15:28:44 -0500

    User can add and delete Notes from Goals

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,13 +1,36 @@
 class NotesController < ApplicationController
-  def create() end
+  before_action :set_note, only: [:update, :destroy]
+  before_action :set_goal, only: [:create, :destroy]
 
-  def update() end
+  def create
+    @goal.notes.create!(note_params)
+    redirect_to_goal
+  end
 
-  def delete() end
+  def update
+    redirect_to_goal
+  end
+
+  def destroy
+    @note.destroy!
+    redirect_to_goal
+  end
 
   private
 
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+  def set_goal
+    @goal = Goal.find(params[:goal_id])
+  end
+
   def note_params
     params.require(:note).permit(:body)
+  end
+
+  def redirect_to_goal
+    redirect_to @goal
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :goals do
-    resources :notes, only: [:create, :update, :delete]
+    resources :notes, only: [:create, :update, :destroy]
   end
   root 'goals#index'
 end
