eaa937f27b340d54ed1b7b5254256ec760138497
Jesse Ellis <jessetellis@gmail.com>
2017-01-09 13:21:59 -0500

    Related items destroyed

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -5,5 +5,5 @@ class Goal < ApplicationRecord
   validates_presence_of :parent_id, allow_nil: true
   acts_as_tree
   belongs_to :user
-  has_many :notes
+  has_many :notes, dependent: :destroy
 end
